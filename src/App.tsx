9f68e8e805c5bef27495a1100bd4c12868463480
AdhemarMolon <162194104+AdhemarMolon@users.noreply.github.com>
2025-06-28 15:53:24 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-green-600 text-white flex items-center justify-center text-3xl">
+      Hello World com Tailwind!
+    </div>
+  );
+}
+
+// 8. Adiciona Vite config com base para GitHub Pages
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+export default defineConfig({
+  base: "/nome-do-repositorio-no-github/", // <-- MUITO IMPORTANTE!
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
